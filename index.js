207de3abba6972dc286a783c2230d222fbfc839c
kirathenics <bka32129@gmail.com>
2023-05-24 10:13:47 +0300

    added alternative port

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/faculties/lines', Controller.getFacultiesLines)
 app.get('/departments/lines', Controller.getDepartmentsLines)
 app.get('/titles/lines', Controller.getTitlesLines)
 
-app.listen(process.env.PORT, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
     if (err) return console.log(err)
     console.log('Server started!')
 })
